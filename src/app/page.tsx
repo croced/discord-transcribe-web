e06a5884c36eb01982fb5e7c972770c713535036
dancroce <dcroce@outlook.com>
2023-08-14 14:04:34 +0100

    fix: removed special characters from about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,13 +83,13 @@ const Home: React.FC = () => {
         <h1 className="text-2xl font-mono">About</h1>
         <p><b>Discribe</b>, your chatty sidekick in the world of Discord! 🤖</p>
         <br />
-        <p>Tired of straining your ears to catch every hilarious punchline or epic gaming strategy shared in those voice chats? Or perhaps you're a word lover who thrives on the written word and wants to join in on the conversation? Well, fret not, because Discribe is here to save the day! 🦸‍♂️</p>
+        <p>Tired of straining your ears to catch every hilarious punchline or epic gaming strategy shared in those voice chats? Or perhaps you&apos;re a word lover who thrives on the written word and wants to join in on the conversation? Well, fret not, because Discribe is here to save the day! 🦸‍♂️</p>
         <br />
-        <p>🚀 Discribe isn't just any old transcription service – it's a speedy genius! Say goodbye to long waiting times and hello to instant results. With Discribe, those voice messages will be transcribed quicker than you can say "supercalifragilisticexpialidocious" (and yes, it'll transcribe that too!). ⚡️</p>
+        <p>🚀 Discribe isn&apos;t just any old transcription service – it&apos;s a speedy genius! Say goodbye to long waiting times and hello to instant results. With Discribe, those voice messages will be transcribed quicker than you can say &quot;supercalifragilisticexpialidocious&quot; (and yes, it&apos;ll transcribe that too!).</p>
         <br />
         <p>🌈 Discribe is designed with love and accessibility in mind. For our friends who might not be able to hear voice messages, or simply prefer reading, Discribe is the ultimate bridge between the spoken and the written word.</p>
         <br />
-        <p>Using Discribe is as easy as pie. Just invite our bot to your server, and it'll be at your service, ready to transform voice messages into text treasures.</p>
+        <p>Using Discribe is as easy as pie. Just invite our bot to your server, and it&apos;ll be at your service, ready to transform voice messages into text treasures.</p>
       </div>
     </div>
   );
