ad1e8509c7df715f417d4388f916dc567cf877b6
dancroce <dcroce@outlook.com>
2023-08-14 12:32:11 +0100

    feat: prevent indexing and screen reading of backdrop words

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,16 +31,15 @@ const Home: React.FC = () => {
         </div>
 
         {/* Background text */}
-        <div className="absolute inset-0 flex items-center justify-center pointer-events-none backdrop-blur-xl opacity-10 text-white max-w-[100%] blurred-text z-10">
-          {BACKDROP_WORDS}
-        </div>
+        {/* uses aria-hidden and pseudo elements to hide text from screen readers and indexing */}
+        <div aria-hidden="true" id="backdrop-text" className="blurred-text" />
       </div>
 
       {/* SUB-HEADER AREA */}
       <div className="mx-4 h-[5%] flex gap-x-2 justify-between items-center font-mono">
           <div className="flex flex-grow md:flex-grow-0 justify-between rounded-3xl hover:cursor-pointer bg-primary-100 hover:bg-primary-50">
             <a className="flex flex-grow md:flex-grow-0 items-center justify-center py-2 px-8  text-xs md:text-sm">
-              <Image src="/assets/icons/documentation.svg" alt="Documentation" width={20} height={20} className="mr-2" />
+              <Image src="/assets/icons/documentation.svg" alt="Documentation" width={20} height={20} className="mr-2" aria-hidden />
               Documentation
             </a>
           </div>
@@ -48,7 +47,7 @@ const Home: React.FC = () => {
           <p className="text-middle hidden md:block">{ SIGN_IN_HINT }</p>
           <div className="flex flex-grow md:flex-grow-0 justify-between rounded-3xl hover:cursor-pointer bg-primary-100 hover:bg-primary-50">
             <a className="flex flex-grow md:flex-grow-0 items-center justify-center py-2 px-8 text-xs md:text-sm">
-              <Image src="/assets/icons/sign-in.svg" alt="Documentation" width={20} height={20} className="mr-2" />
+              <Image src="/assets/icons/sign-in.svg" alt="Sign in" width={20} height={20} className="mr-2" />
               Sign in
             </a>
           </div>
