4eb38580f5814e45e8729baab9e83503a62a721e
dancroce <dcroce@outlook.com>
2023-08-18 23:42:08 +0100

    fix: attempted fix for DYNAMIC_SERVER_USAGE

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,5 @@
-import { Database } from '@/lib/database.types';
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+import Dashboard from './Dashboard';
 
-export default async function Dashboard() {
-	const supabase = createServerComponentClient<Database>({ cookies });
-
-	const {
-		data: { session }
-	} = await supabase.auth.getSession();
-
-	if (!session) {
-		redirect('/');
-	}
-
-	const { data } = await supabase.from('posts').select('*');
-
-	return <pre>{JSON.stringify({ data }, null, 2)}</pre>;
+export default function WebApp() {
+	return <Dashboard />;
 }
